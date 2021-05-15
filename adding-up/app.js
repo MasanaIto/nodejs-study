b36c2dfceb5e6784a1d3cf6b217be349d516be47
MasanaIto <z3heng0326@gmail.com>
2021-05-15 18:00:10 +0900

    [集計処理]人口の減少率ランキングを作成

--- adding-up/app.js
+++ adding-up/app.js
@@ -50,14 +50,12 @@ rl.on('close', () => {
     // 変化率ごとに並び替える
     // Array.from()...Mapを普通の配列に変換
     const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
-        return pair2[1].change - pair1[1].change;
+        return pair1[1].change - pair2[1].change;
     });
     
     // map関数[配列.map(関数)]...Arrayの要素それぞれを、与えられた関数を適用した内容に変換する
-    const rankingStrings = rankingArray.map(([key, value]) => {
-        return (
-            key + ': ' + value.popu10 + '=>' + value.popu15 + ' 変化率:' + value.change
-        );
+    const rankingStrings = rankingArray.map(([key, value], i) => {
+        return (i + 1) + '位 ' + key + ': ' + value.popu10 + '=>' + value.popu15 + ' 変化率:' + value.change;
     });
     console.log(rankingStrings);
 });
