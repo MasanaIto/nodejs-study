dfda1182d371da30fb203914bb04219f758ffc6f
MasanaIto <z3heng0326@gmail.com>
2021-05-15 16:54:43 +0900

    [集計処理]ファイルからデータを抜き出す
    2010年と2015年のデータから「集計年」「都道府県」「15〜19 歳の人口」を抜き出す

--- adding-up/app.js
+++ adding-up/app.js
@@ -9,6 +9,19 @@ const rs = fs.createReadStream('./popu-pref.csv');
 const rl = readline.createInterface({ input: rs, output: {} });
 
 // rlオブジェクトでlineイベントが発生したら実行
+// 2010 年と 2015 年の際の集計年、都道府県、15〜19 歳の人口がコンソール上に出力
 rl.on('line', lineString => {
-    console.log(lineString);
+    const columns = lineString.split(',');
+    // 集計年 parseInt...文字列を整数値に変換する関数
+    const year = parseInt(columns[0]);
+    // 都道府県
+    const prefecture = columns[1];
+    // 15〜19 歳の人口
+    const popu = parseInt(columns[3]);
+
+    if (year === 2010 || year === 2015) {
+        console.log(year);
+        console.log(prefecture);
+        console.log(popu);
+    }
 });
