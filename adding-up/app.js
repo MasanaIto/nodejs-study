8c2c2ccb85a163213d332c54750010769b68e79f
MasanaIto <z3heng0326@gmail.com>
2021-05-15 17:36:27 +0900

    [集計処理]人口の変化率を計算
    2015年の2010年に対する変化率を算出する

--- adding-up/app.js
+++ adding-up/app.js
@@ -2,11 +2,12 @@
 // モジュール呼び出し
 const fs = require('fs');
 const readline = require('readline');
-
 // csvファイルから読み込みを行うStreamを生成
 const rs = fs.createReadStream('./popu-pref.csv');
 // readlineオブジェクトのinputとして設定し、rlオブジェクトを作成
 const rl = readline.createInterface({ input: rs, output: {} });
+// key: 都道府県 value: 集計データのオブジェクト
+const prefectureDataMap = new Map();
 
 // rlオブジェクトでlineイベントが発生したら実行
 // 2010 年と 2015 年の際の集計年、都道府県、15〜19 歳の人口がコンソール上に出力
@@ -20,8 +21,29 @@ rl.on('line', lineString => {
     const popu = parseInt(columns[3]);
 
     if (year === 2010 || year === 2015) {
-        console.log(year);
-        console.log(prefecture);
-        console.log(popu);
+        let value = prefectureDataMap.get(prefecture);
+        if (!value) {
+            value = {
+                popu10: 0,
+                popu15: 0,
+                change: null
+            };
+        }
+        // 人口のデータを連想配列に保存
+        if (year === 2010) {
+            value.popu10 = popu;
+        }
+        if (year === 2015) {
+            value.popu15 = popu;
+        }
+        prefectureDataMap.set(prefecture, value);
     }
+});
+
+rl.on('close', () => {
+    for (const [key, value] of prefectureDataMap) {
+        // 人口の変化率
+        value.cahnge = value.popu15 / value.popu10;
+    }
+    console.log(prefectureDataMap);
 });
